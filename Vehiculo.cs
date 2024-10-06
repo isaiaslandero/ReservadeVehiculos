39bff916ea6bb818a97fdec209b4cedcc223f65d
Isaias Landero <isaiaslandero8@gmail.com>
2024-10-06 13:22:01 -0600

    Creacion de la Clase Vehiculos

---
+++ Vehiculo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReservadeVehiculos
+{
+	internal class Vehiculo
+	{
+		private string modelo {  get; set; }	
+		private double precio { get; set; }	
+
+		public Vehiculo() {
+		
+		}
+	}
+}
